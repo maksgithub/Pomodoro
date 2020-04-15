3a1eb704afb9378ad9730e0b6c0614055468ec92
Maksym Tatsenko <maksym.tatsenko@skylum.com>
2020-04-15 22:34:01 +0300

    Add

---
+++ src/Pomodoro/Pomodoro.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pomodoro.Core;
+
+namespace Pomodoro.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            Assert.IsNotNull(c);
+        }
+    }
+}
